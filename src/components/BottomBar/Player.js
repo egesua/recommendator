014bec56bac7e976e0350ae1d09deefd98cd8125
egesua <egesu06@gmail.com>
2024-01-25 23:04:50 +0300

    updates

--- src/components/BottomBar/Player.js
+++ src/components/BottomBar/Player.js
@@ -1,18 +1,22 @@
 import { Icon } from "Icons"
-import { Range, getTrackBackground } from "react-range";
-import { useState } from "react";
-
-const STEP = 0.1;
-const MIN = 0;
-const MAX = 100;
+import { useAudio } from "react-use";
+import { secondsToTime } from "utils";
+import CustomRange from "components/CustomRange";
 
 
 
 function Player() {
-  const [values, setValues] = useState([50])
+
+  const [audio, state, controls, ref] = useAudio({
+    src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+    autoPlay: true,
+  });
+
     return (
       <div className=" flex px-4 justify-between items-center h-full">
-        <div className="min-w-[11.25rem] w-[30%]">sol</div>
+        <div className="min-w-[11.25rem] w-[30%]">
+          sol
+        </div>
         <div className="max-w-[45.125] w-[40%] flex flex-col items-center">
           <div className="flex items-center gap-x-2">
             <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
@@ -21,8 +25,8 @@ function Player() {
             <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
               <Icon size={16} name="playerPrev" />
             </button>
-            <button className="w-8 h-8 bg-white flex items-center justify-center text-black rounded-full hover:scale-[1.06]">
-              <Icon size={16} name="play" />
+            <button onClick={state?.playing ? controls.pause : controls.play} className="w-8 h-8 bg-white flex items-center justify-center text-black rounded-full hover:scale-[1.06]">
+              <Icon size={16} name={state?.playing ? 'pause' : 'play'} />
             </button>
             <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
               <Icon size={16} name="playerNext" />
@@ -31,52 +35,28 @@ function Player() {
               <Icon size={16} name="repeat" />
             </button>
           </div>
-          <div className="w-full">
-            <Range
-              values={values}
-              step={STEP}
-              min={MIN}
-              max={MAX}
-              onChange={(values) => setValues( values )}
-              renderTrack={({ props, children }) => (
-                <div
-                  onMouseDown={props.onMouseDown}
-                  onTouchStart={props.onTouchStart}
-                  className="w-full h-7 flex group:"
-                  style={{
-                    ...props.style,
-                  }}
-                >
-                  <div
-                    ref={props.ref}
-                    className="h-1 w-full rounded-md self-center"
-                    style={{
-                      background: getTrackBackground({
-                        values: values,
-                        colors: ["#1db954", "#535353"],
-                        min: MIN,
-                        max: MAX,
-                      }),
-                    }}
-                  >
-                    {children}
-                  </div>
-                </div>
-              )}
-              renderThumb={({ props, isDragged }) => (
-                <div
-                  {...props}
-                  className={`h-3 w-3 rounded-full bg-white ${!isDragged ? 'opacity-0' : ''} group-hover:opacity-100`}
-                  style={{
-                    ...props.style,
-                    boxShadow: "0px 2px 4px 0 rgb(0 0 0 / 50%)",
-                  }}
-                />
-              )}
-            />
+          <div className="w-full flex items-center gap-x-2">
+            {audio}
+            <div>
+              {secondsToTime(state?.time)}
+            </div>
+            <CustomRange
+              step={0.1}
+              min={0}
+              max={state?.duration || 1}
+              value={state?.time}
+              onChange={value => controls.seek(value)}
+             />
+            <div>
+              {secondsToTime(state?.duration)}
+            </div>
           </div>
         </div>
-        <div className="min-w-[11.25rem] w-[30%] flex justify-end">sag</div>
+        <div className="min-w-[11.25rem] w-[30%] flex items-center justify-end">
+        <button className="w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100">
+              <Icon size={16} name="normalVol" />
+            </button>
+        </div>
       </div>
     );
 }
